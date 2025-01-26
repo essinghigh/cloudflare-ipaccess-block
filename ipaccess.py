8c7a9ed1521c043a93bdf606b1b46210704b464b
Henry Essinghigh <henry@essinghigh.dev>
2025-01-26 12:46:33 +0000

    Update ipaccess.py

--- ipaccess.py
+++ ipaccess.py
@@ -7,7 +7,7 @@ client = Cloudflare(api_email=os.environ["CF_API_EMAIL"], api_key=os.environ["CF
 def filter_subnets(content):
     return [line.strip() for line in content.splitlines() if line and not line.startswith('#') and int(line.split('/')[1]) <= 31]
 
-url = 'https://github.com/essinghigh/blocklist-abuseipdb-compressed/raw/refs/heads/main/abuseipdb-s100-120d_compressed.ipv4'
+url = 'https://github.com/essinghigh/blocklist-abuseipdb-compressed/raw/refs/heads/main/abuseipdb-s100-60d_compressed.ipv4'
 response = requests.get(url)
 response.raise_for_status()
 
